6c24c310da8da05a256f61982737573a18a437a3
asanchezgiraldo <alejandro.sanchezgiraldo@vodafone.com.au>
2021-03-05 19:42:24 +1100

    recomendations endpoint and prices

--- app/app.js
+++ app/app.js
@@ -3,6 +3,16 @@ const cspub = require('./coinpub/checkLatest');
 const app = express()
 const port = process.env.PORT || 3000;
 
+let request = require('request');
+let hasurasec = process.env.hasurasec;
+let hasurauri = 'https://covid19-logic.herokuapp.com/v1/graphql';
+let opt = {'method': 'POST',
+    'url': hasurauri,
+    'headers': {
+      'x-hasura-admin-secret': '' + hasurasec +'',
+      'Content-Type': 'application/json'
+    }};
+
 app.get('/', (req, res) => {
   res.send('<a href=https://github.com/ale-sanchez-g/coinspot-bot/blob/main/README.md>GitHub Project</a>')
 })
@@ -16,4 +26,35 @@ app.get('/prices', (req, res) => {
     res.send(data);
   });
   
+})
+
+app.get('/recomend/:coin', (req, res) => {
+
+  let coinName = req.params.coin;
+
+    qry = {
+        body: JSON.stringify({
+          query: `query avaragecost {
+            coins_aggregate(distinct_on: coin, where: {coin: {_eq: "${coinName}"}}) {
+              aggregate {
+                avg {
+                  last
+                }
+              }
+            }
+            coins(limit: 5, order_by: {created_at: desc}, where: {coin: {_eq: "${coinName}"}}) {
+              coin
+              last
+            }
+          }`,
+          variables: {}
+        })
+      };
+
+    options = JSON.parse((JSON.stringify(opt) + JSON.stringify(qry)).replace(/}{/g,","));
+
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+      res.end(response.body);
+    });  
 })
---
+++ app/logic/hasura.js
@@ -0,0 +1,72 @@
+let request = require('request');
+let hasurasec = process.env.hasurasec;
+let hasurauri = 'https://covid19-logic.herokuapp.com/v1/graphql';
+
+let opt = {'method': 'POST',
+    'url': hasurauri,
+    'headers': {
+      'x-hasura-admin-secret': '' + hasurasec +'',
+      'Content-Type': 'application/json'
+    }};
+
+function coinLogic(data) {
+    console.log(data);
+}
+
+coinLogic.pushCoinsData = function(data) {
+    for(let attributename in data){    
+        qry = {
+          body: JSON.stringify({
+            query: `mutation MyMutation {
+          insert_coins(objects: {ask: ${data[attributename].ask}, bid: ${data[attributename].bid}, coin: "${attributename}", last: ${data[attributename].last}}) {
+            returning {
+              id
+              created_at
+            }
+          }
+        }`,
+            variables: {}
+          })
+        };
+        
+        options = JSON.parse((JSON.stringify(opt) + JSON.stringify(qry)).replace(/}{/g,","));
+
+        request(options, function (error, response) {
+          if (error) throw new Error(error);
+          console.log(response.body);
+        });
+      }  
+};
+
+coinLogic.recommendation = function(coin) {
+    console.log(`checking coin ${coin}`);
+
+    qry = {
+        body: JSON.stringify({
+          query: `query avaragecost {
+            coins_aggregate(distinct_on: coin, where: {coin: {_eq: "${coin}"}}) {
+              aggregate {
+                avg {
+                  last
+                }
+              }
+            }
+            coins(limit: 5, order_by: {created_at: desc}, where: {coin: {_eq: "${coin}"}}) {
+              coin
+              last
+            }
+          }`,
+          variables: {}
+        })
+      };
+
+    options = JSON.parse((JSON.stringify(opt) + JSON.stringify(qry)).replace(/}{/g,","));
+
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+      console.log(response.body);
+    });
+
+};
+
+module.exports = coinLogic;
